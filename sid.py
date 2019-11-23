7490479379fa97416cda3f362418f062cbaa712b
eoin <eoinoreilly30@gmail.com>
2019-11-23 19:31:37 +0000

    f

--- sid.py
+++ sid.py
@@ -1,8 +1,11 @@
 import csv
 import os
-from data_generator import DataGenerator
 import tensorflow as tf
-from keras.layers import Input, Conv2D, MaxPooling2D, Conv2DTranspose, Concatenate
+
+from data_generator import DataGenerator
+from subpixel import SubpixelConv2D
+from keras import Model
+from keras.layers import Input, Conv2D, MaxPooling2D, Lambda, Conv2DTranspose, concatenate
 
 checkpoint_dir = '.\\result_Sony\\'
 result_dir = '.\\result_Sony\\'
@@ -27,15 +30,21 @@ data_gen_params = {'input_dim': (2120, 1416),  # 4240/2, 2832/2
 training_generator = DataGenerator(input_image_paths, gt_image_dict, **data_gen_params)
 
 
-def upsample_and_concat(x1, x2, input_channels, output_channels):
-    pool_size = 2
-    deconv_filter = tf.Variable(tf.truncated_normal([pool_size, pool_size, output_channels, input_channels], stddev=0.02))
-    deconv = tf.nn.conv2d_transpose(x1, deconv_filter, tf.shape(x2), strides=[1, pool_size, pool_size, 1])
-
-    deconv_output = tf.concat([deconv, x2], 3)
-    deconv_output.set_shape([None, None, None, output_channels * 2])
-
-    return deconv_output
+# def UpsampleAndConcat(x1, x2, input_channels, output_channels):
+#     def upsample_and_concat(x):
+#         pool_size = 2
+#         deconv_filter = tf.Variable(tf.truncated_normal([pool_size, pool_size, output_channels, input_channels], stddev=0.02))
+#         deconv = tf.nn.conv2d_transpose(x1, deconv_filter, tf.shape(x2), strides=[1, pool_size, pool_size, 1])
+#
+#         deconv_output = tf.concat([deconv, x2], 3)
+#         deconv_output.set_shape([None, None, None, output_channels * 2])
+#
+#         return deconv_output
+#
+#     def output_shape():
+#         return [None, None, None, output_channels * 2]
+#
+#     return Lambda(upsample_and_concat, output_shape=output_shape, name='upsample_and_concat')
 
 
 # functional model
@@ -67,11 +76,31 @@ conv5 = Conv2D(512, (3, 3), activation="relu")(conv5)
 # Conv2DTranspose(256, (3, 3), activation="relu")
 # concatenate(axis=3)
 
-up6 = upsample_and_concat(conv5, conv4, 512, 256)
-conv6 = Conv2D(256, (3, 3), activation="relu")(up6)
+up6 = Conv2DTranspose(256, (3, 3), strides=(2, 2))(conv5)
+concat6 = concatenate([up6, conv4], axis=3)
+conv6 = Conv2D(256, (3, 3), activation="relu")(concat6)
 conv6 = Conv2D(256, (3, 3), activation="relu")(conv6)
 
+up7 = Conv2DTranspose(128, (3, 3), strides=(2, 2))(conv6)
+concat7 = concatenate([up7, conv3], axis=3)
+conv7 = Conv2D(128, (3, 3), activation="relu")(concat7)
+conv7 = Conv2D(128, (3, 3), activation="relu")(conv7)
 
+up8 = Conv2DTranspose(64, (3, 3), strides=(2, 2))(conv7)
+concat8 = concatenate([up8, conv2], axis=3)
+conv8 = Conv2D(64, (3, 3), activation="relu")(concat8)
+conv8 = Conv2D(64, (3, 3), activation="relu")(conv8)
 
+up9 = Conv2DTranspose(32, (3, 3), strides=(2, 2))(conv8)
+concat9 = concatenate([up9, conv1], axis=3)
+conv9 = Conv2D(32, (3, 3), activation="relu")(concat9)
+conv9 = Conv2D(32, (3, 3), activation="relu")(conv9)
 
+conv10 = Conv2D(12, (1, 1))(conv9)
+
+output_layer = SubpixelConv2D(conv10.shape, 2)(conv10)
+
+model = Model(input=input_layer, output=conv10)
+
+model.summary()
 
